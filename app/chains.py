d053491679d87753eef54f93b20f707e2e64ef17
Pothamsetti Radha Krishna <radhapothamsetti@gmail.com>
2025-09-16 07:50:48 +0530

    Update chains.py

--- app/chains.py
+++ app/chains.py
@@ -39,14 +39,14 @@ class Chain:
             {job_description}
 
             ### INSTRUCTION:
-            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
+            You are Radha krishna, a business development executive at R.k solutions. R.k solutions is an AI & Software Consulting company dedicated to facilitating
             the seamless integration of business processes through automated tools. 
             Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
             process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
+            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of R.k solutions 
             in fulfilling their needs.
-            Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-            Remember you are Mohan, BDE at AtliQ. 
+            Also add the most relevant ones from the following links to showcase R.k solutions's portfolio: {link_list}
+            Remember you are Mohan, BDE at R.k solutions. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
 
@@ -57,4 +57,5 @@ class Chain:
         return res.content
 
 if __name__ == "__main__":
+
     print(os.getenv("GROQ_API_KEY")) 
